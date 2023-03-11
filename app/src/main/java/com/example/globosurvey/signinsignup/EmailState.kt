f5880b9fc62a90957ec198a1416688fc88494c9f
asino-nelson <asinonelson276@gmail.com>
2023-03-11 22:16:16 +0300

    Email state

---
+++ app/src/main/java/com/example/globosurvey/signinsignup/EmailState.kt
@@ -0,0 +1,29 @@
+package com.example.globosurvey.signinsignup
+
+import androidx.compose.ui.text.input.TextFieldValue
+import java.util.regex.Pattern
+
+private const val EMAIL_VALIDATION_REGEX = "(.+)@(.+)\$"
+
+
+class EmailState(val email:String? = null):
+    TextFieldState(
+        validator = ::isEmailValid,
+        errorFor = ::emailValidationError
+    ){
+    init {
+        email?.let {
+            text = it
+        }
+    }
+}
+
+private fun emailValidationError(email:String):String{
+    return "Invalid email: $email"
+}
+
+private fun isEmailValid(email:String):Boolean{
+    return Pattern.matches(EMAIL_VALIDATION_REGEX,email)
+}
+
+val EmailStateSaver = textFieldStateSaver(EmailState())
